1270dbfa2144ade8f747e83aca0854ffce5807a4
MrPlum <plumlabs.dev@gmail.com>
2020-01-14 15:01:23 -0300

    Para el Mati que lo mira por TV

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  post '/graphql', to: 'graphql#execute'
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
-  post "/graphql", to: "graphql#execute"
+
   root to: "home#index"
   get 'profile', to: 'users#show'
   resources :companies
