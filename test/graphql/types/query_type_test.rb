3a0a2f0792b0e1b24f39c8630d1284be2c275400
matias <mformento8@gmail.com>
2020-01-16 12:31:34 -0300

    add test not run

---
+++ test/graphql/types/query_type_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+module Types
+	class QueryTypeTest < Types::QueryType 
+		test 'all project' do
+			author = create(:one)
+			non_author = create(:non_user)
+			name_project = create(:project, name: true, author: author)
+			
+			query_string = <<-GRAPHQL
+			query($id: ID!) {
+				node(id: $id) {
+					... on project {
+						name
+						repository
+						user {
+							name
+						}
+						team{
+							name
+						}
+						company {
+							name
+						}
+					}
+				}
+			}
+			GRAPHQL
+
+			project_id = PlockSchema.id_from_object(Project, Types::ProjectType, {})
+
+			author_result = MySchema.execute(query_string,
+				context: { viewer: author },
+				variables: { id: post_id }
+			)
+			assert_equal "roberto", author_result["data"]["node"]["name"]
+			non_author_result = MySchema.execute(query_string,
+				context: { viewer: non_author },
+				variables: { id: project_id }
+			)
+			assert_nil author_result["data"]["node"]
+		end
+	end
+end
